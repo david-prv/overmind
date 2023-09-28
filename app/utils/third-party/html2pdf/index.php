ab27665ae3c8cb5059cc98bcd4a826fad6ab29ac
david-prv <david_dewes@web.de>
2023-09-28 13:46:35 +0200

    - Changed icon (circle one was too clumsy)

--- app/utils/third-party/html2pdf/index.php
+++ app/utils/third-party/html2pdf/index.php
@@ -320,8 +320,8 @@ $offerComment = $data["offer_comment"];
     window.onload = async function () {
         await html2pdf().set(options).from(makepdf).save();
         makepdf.style.display = "none";
-        spinner.parentElement.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"50\" height=\"50\" fill=\"currentColor\" class=\"bi bi-check-circle-fill\" viewBox=\"0 0 16 16\">\n" +
-            "  <path d=\"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z\"/>\n" +
+        spinner.parentElement.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"50\" height=\"50\" fill=\"currentColor\" class=\"bi bi-check-lg\" viewBox=\"0 0 16 16\">\n" +
+            "  <path d=\"M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z\"/>\n" +
             "</svg>";
     };
 </script>
