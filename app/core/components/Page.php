c298b1cf15908f224d7a728d71503ac6f2a0cb72
David <66866223+david-prv@users.noreply.github.com>
2023-10-09 18:42:49 +0200

    - small adjustment to main.css

--- app/core/components/Page.php
+++ app/core/components/Page.php
@@ -221,9 +221,9 @@ class Page
     public function _injectNoMobile(): void
     {
         $noScript = <<<HTML
-            <div class="no-mobile-notice">
+            <div class="no-mobile-notice" style="display:none;">
                 <style>
-                    .no-script {
+                    .no-mobile-notice-message {
                         padding-top:10%!important;
                         height:100vh;
                         width:100vw;
@@ -234,7 +234,7 @@ class Page
                         text-align: center;
                     }
                 </style>
-                <div class="no-script">
+                <div class="no-mobile-notice-message">
                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-exclamation-octagon-fill" viewBox="0 0 16 16">
                       <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                     </svg>
